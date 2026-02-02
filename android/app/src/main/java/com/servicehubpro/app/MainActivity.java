e683e03adc1ee6051f18625284d4f73a5c157d1d
humayanfarid046 <humayanfarid046@gmail.com>
2026-02-02 13:20:37 +0000

    Added Android project

---
+++ android/app/src/main/java/com/servicehubpro/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.servicehubpro.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.servicehubpro.app',
+  appName: 'Service-HubPro',
+  webDir: 'dist'
+};
+
+export default config;
